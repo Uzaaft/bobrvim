da886286ea8ce6b61681a62bd167ec4682216335
Uzair Aftab <git@uzaaft.me>
2026-02-28 23:08:05 +0100

    fix: disable mason installer

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,6 +1,7 @@
 local disabled = {
   "jay-babu/mason-null-ls.nvim",
   "nvimtools/none-ls.nvim",
+  "WhoIsSethDaniel/mason-tool-installer.nvim",
 }
 
 return vim.tbl_map(function(plugin) return { plugin, enabled = false } end, disabled)
