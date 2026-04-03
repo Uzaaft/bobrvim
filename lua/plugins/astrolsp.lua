27532553b46f58bfaecb8325c6c87f2a902baad0
Uzair Aftab <git@uzaaft.me>
2026-04-03 12:05:37 +0200

    lsp: add nixd

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,4 +1,4 @@
-local servers = { "tsgo" } -- only add local servers if their commands are available
+local servers = { "tsgo", "nixd" } -- only add local servers if their commands are available
 for server, cmd in pairs { rust_analyzer = "rust-analyzer" } do
   if vim.fn.executable(cmd) == 1 then table.insert(servers, server) end
 end
